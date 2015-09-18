a36dfe709e67ff8337bbbfd1fb31464ace77dd77
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-18 08:21:43 -0400

    adds gulp for coverage

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var jshint = require('gulp-jshint');
+require('jshint-stylish');
+
+gulp.task('coverage', function () {
+  return gulp.src('index.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('mocha', ['coverage'], function () {
+  return gulp.src('test/*.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports());
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('index.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default', ['mocha', 'jshint']);
